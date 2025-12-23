47060b1c8fb18ab2e7cc57e8bd9226ab2c810bb9
Aren Baghramian <contact@anbtech.xyz>
2025-12-22 20:43:09 -0800

    Loading screens added

--- app/dashboard/loading.tsx
+++ app/dashboard/loading.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
-
-const LoadingPage = () => {
+export default function Loading() {
     return (
-        <div>
-            <p>Im loading!</p>
+        <div className="flex items-center justify-center h-screen bg-neutral-900">
+            <div className="flex space-x-2">
+                <div className="w-3 h-3 bg-gray-300 rounded-full animate-bounce delay-0"></div>
+                <div className="w-3 h-3 bg-gray-400 rounded-full animate-bounce delay-50"></div>
+                <div className="w-3 h-3 bg-gray-500 rounded-full animate-bounce delay-150"></div>
+            </div>
         </div>
     );
-};
-
-export default LoadingPage;
+}
--- app/ticker/[symbol]/loading.tsx
+++ app/ticker/[symbol]/loading.tsx
@@ -1,7 +1,11 @@
 export default function Loading() {
     return (
-        <div>
-            <h1>Im loading!</h1>
+        <div className="flex items-center justify-center h-screen bg-neutral-900">
+            <div className="flex space-x-2">
+                <div className="w-3 h-3 bg-gray-300 rounded-full animate-bounce delay-0"></div>
+                <div className="w-3 h-3 bg-gray-400 rounded-full animate-bounce delay-50"></div>
+                <div className="w-3 h-3 bg-gray-500 rounded-full animate-bounce delay-150"></div>
+            </div>
         </div>
     );
 }
